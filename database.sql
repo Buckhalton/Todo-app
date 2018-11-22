23fbf4a866e946336720faa2835dc767af88426a
Isaiah Buckhalton <Isaiah.Buckhalton@gmail.com>
2018-11-22 10:37:16 -0600

    created database table.

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE todo (
+    id SERIAL PRIMARY KEY,
+    task VARCHAR(200),
+    priority FLOAT,
+    completed BOOLEAN
+);
+
+INSERT INTO todo (task, completed, priority)
+VALUES('Clean the house', false, 3),
+('Buy groceries', false, 1),
+('Walk the dog', false, 2),
+('Finish Project', false, 1),
+('Work out', false, 1);
